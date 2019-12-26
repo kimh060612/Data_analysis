1b81f4e4eddf41d7af4ce36f64fc4cd33c725ca9
Michael <kimh060612@naver.com>
2019-12-26 21:22:54 +0900

    .

--- galaxy_classification/main.py
+++ galaxy_classification/main.py
@@ -1,52 +1,68 @@
 import tensorflow as tf
 import keras as K
+from keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 from sklearn.model_selection import train_test_split
 import os
 import cv2
 
-data_path = "./Original_data/train/"
-data_path_test = "./Original_data/test/"
+data_path = "./Resize_data/train_galaxy/"
+data_path_test = "./Resize_data/test_galaxy/"
 data_list = os.listdir(data_path)
 data_list_test = os.listdir(data_path_test)
 Img_data = []
-label = []
+Img_data_test = []
+
+dataGen = ImageDataGenerator(rotation_range=40, width_shift_range=0.2, height_shift_range= 0.2, rescale=1./255, shear_range=0.2,zoom_range=0.2,horizontal_flip=True,fill_mode="nearest")
 
 Y = []
-onehot = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+Y_test = []
+onehot = [[1,0,0],[0,1,0],[0,0,1]]
 
-for con in data_list:
-    name = con.split(".")[0]
-    label.append(name)
+for i in range(len(data_list)):
+    img_path = data_path + data_list[i] + "/"
+    img_list = os.listdir(img_path)
+    for j in range(len(img_list)):
+        Y.append(onehot[i])
+        image_train = cv2.imread(img_path + img_list[j], cv2.IMREAD_GRAYSCALE)
+        Img_data.append(image_train.tolist())
 
-label = list(set(label))
-print(label)
+for i in range(len(data_list_test)):
+    img_path = data_path_test + data_list_test[i] + "/"
+    img_list = os.listdir(img_path)
+    for j in range(len(img_list)):
+        Y_test.append(onehot[i])
+        image_test = cv2.imread(img_path + img_list[j], cv2.IMREAD_GRAYSCALE)
+        Img_data_test.append(image_test.tolist())
 
-for con in data_list:
-    name = con.split(".")[0]
-    Y.append(onehot[label.index(name)])
-    image_train = cv2.imread(data_path+con, cv2.IMREAD_GRAYSCALE)
-    Img_data.append(image_train)
+Y = np.array(Y).reshape((-1,3))
+Img_data = np.array(Img_data).reshape((-1,50,50,1))
 
-print(Img_data)
+Img_data = dataGen.flow_from_dataframe(Img_data, target_size=(50,50), batch_size= 32, class_mode="binary")
 
-'''
+Y_test = np.array(Y_test).reshape((-1,3))
+Img_data_test = np.array(Img_data_test).reshape((-1,50,50,1))
+print(Img_data.shape)
+
+
 model = K.models.Sequential()
-model.add(K.layers.Conv2D(32, (3,3), activation="relu", input_shape=(300,300,1)))
+model.add(K.layers.Conv2D(32, (3,3), activation="relu", input_shape=(50,50,1)))
+model.add(K.layers.MaxPooling2D(2,2))
+model.add(K.layers.Conv2D(64, (3,3), activation="relu"))
 model.add(K.layers.MaxPooling2D(2,2))
 model.add(K.layers.Conv2D(64, (3,3), activation="relu"))
 model.add(K.layers.MaxPooling2D(2,2))
 model.add(K.layers.Conv2D(64, (3,3), activation="relu"))
 model.add(K.layers.Flatten())
+model.add(K.layers.Dropout(0.4))
 model.add(K.layers.Dense(64, activation='relu'))
-model.add(K.layers.Dense(10, activation='softmax'))
+model.add(K.layers.Dense(3, activation='softmax'))
 model.summary()
 
-model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=['accuracy'])
+model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=['accuracy'])
 
-model.fit(X_train, Y_train, epochs=100)
+model.fit(Img_data, Y, epochs=50)
 
-test_loss, test_acc = model.evaluate(X_test, Y_test, verbose=2)
+test_loss, test_acc = model.evaluate(Img_data_test,  Y_test, verbose=2)
 
-print("Test Accuarcy:", test_acc*100, "%")
-'''
+print(test_acc*100, "% ", "Finish")
