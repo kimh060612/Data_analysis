c2e8a6555a6664cacc1d81273a6f6f2176f2e098
kimh060612 <kimh060612@naver.com>
2021-04-11 11:24:41 +0900

    .

---
+++ CellData/model/model.py
@@ -0,0 +1,16 @@
+from torch import nn
+
+
+class FCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear = nn.Linear()
+        
+        
+    def __init_weights(self):
+
+        return super().__init_subclass__()
+
+    def forward(self, x):
+
+        pass
