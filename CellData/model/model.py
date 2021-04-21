9face88302bd120528719d5cb12ddcb58523bd86
kimh060612 <kimh060612@naver.com>
2021-04-21 23:15:43 +0900

    .

--- CellData/model/model.py
+++ CellData/model/model.py
@@ -8,15 +8,15 @@ torch.manual_seed(777)
 if device == 'cuda':
     torch.cuda.manual_seed_all(777)
 
-visionTransformer = ViT(
-    image_size=256,
-    patch_size=16,
-    num_classes=19,
-    channels=4,
+visionTransformer = v = ViT(
+    image_size = 256,
+    patch_size = 32,
+    num_classes = 19,
     dim = 1024,
     depth = 6,
     heads = 16,
     mlp_dim = 2048,
     dropout = 0.1,
-    emb_dropout = 0.1
+    emb_dropout = 0.1,
+    channels=4
 ).to(device)
