9dbc00e01a3d9644bfccec8a355d2923b6be86b4
kimh060612 <kimh060612@naver.com>
2021-05-08 12:22:00 +0900

    .

--- CellData/Inference.py
+++ CellData/Inference.py
@@ -42,11 +42,12 @@ for i in tqdm(range(len(TestData_Hash))):
     testImg = np.array([testImg])
     result = visionTransformer(torch.Tensor(testImg))
     result = torch.where(result > 0.6, ones, zeros).tolist()
-    pred = ' '.join(result)
+    pred = [ j for j in range(len(result)) if result[j] == 1 ]
+    prediction = ' '.join(pred)
     Submission['ID'].append(TestData_Hash[i])
     Submission['ImageWidth'].append(TestData_Size[TestData_Hash[i]][0])
     Submission['ImageHeight'].append(TestData_Size[TestData_Hash[i]][1])
-    Submission['PredictionString'].append(pred)
+    Submission['PredictionString'].append(prediction)
 
 SubmissionCSV = pd.DataFrame(Submission)
 SubmissionCSV.to_csv(submission_path)
